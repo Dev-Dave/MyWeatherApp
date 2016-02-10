e2eb36b111e69cccf01d3d15e88823c0a1447bd3
Dev-Dave <Dev-Dave@users.noreply.github.com>
2016-02-10 16:03:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dcaouette/myweatherapp/MainActivityFragment.java
@@ -0,0 +1,55 @@
+package com.example.dcaouette.myweatherapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String[] forecastArray = {
+                "Monday - snow",
+                "Tuesday - snow",
+                "Wednesday - snow",
+                "Thursday - snow",
+                "Friday - snow",
+                "Saturday - snow",
+                "Sunday - snow"
+        };
+
+        List<String> forecastList = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                forecastArray
+        );
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
+    }
+}
